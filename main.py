47908a249a0146b2e4a63d46186540cf4b22a43f
afertile <makszemskow@gmail.com>
2024-08-05 13:18:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+print("1st program")
+print((9 ** 0.5) * 5)
+print("2nd program")
+print(9.99 > 9.98 and 1000 != 1000.1)
+print("3rd program")
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print(a)
+print(b)
+print(a == b)
+print("4th program")
+stroka = '123.456'
+stroka = float(stroka)
+stroka *= 10
+stroka = int(stroka)
+stroka %= 10
+print(stroka)
